2602e51cfef706e4d44f2904d3a93c6f54b84393
igorsaux <igorsa.ux@gmail.com>
2026-05-20 18:49:29 +0300

    refactor inputs

---
+++ scripts/game_inputs.gd
@@ -0,0 +1,12 @@
+# Copyright (C) 2026 Igor Spichkin
+# SPDX-License-Identifier: MPL-2.0
+
+extends Node
+
+const CAM_ZOOM_IN := "cam_zoom_in"
+const CAM_ZOOM_OUT := "cam_zoom_out"
+const CAM_ROTATE_HOLD := "cam_rotate_hold"
+const CAM_ROT_LEFT := "cam_rot_left"
+const CAM_ROT_RIGHT := "cam_rot_right"
+const CAM_ROT_UP := "cam_rot_up"
+const CAM_ROT_DOWN := "cam_rot_down"
---
+++ scripts/settings_manager.gd
@@ -0,0 +1,46 @@
+# Copyright (C) 2026 Igor Spichkin
+# SPDX-License-Identifier: MPL-2.0
+
+extends Node
+
+const SETTINGS_FILE := "user://settings.cfg"
+const INPUT_SECTION := "input"
+
+func _ready() -> void:
+	load_settings()
+
+func load_settings() -> void:
+	var config = ConfigFile.new()
+	
+	if config.load(SETTINGS_FILE) != OK:
+		return
+		
+	if config.has_section(INPUT_SECTION):
+		for action in config.get_section_keys(INPUT_SECTION):
+			if not InputMap.has_action(action):
+				continue
+				
+			var events_str = config.get_value(INPUT_SECTION, action)
+			var events = str_to_var(events_str)
+			
+			if events is Array:
+				InputMap.action_erase_events(action)
+				
+				for event in events:
+					InputMap.action_add_event(action, event)
+
+func save_settings() -> void:
+	var config = ConfigFile.new()
+	
+	for action in InputMap.get_actions():
+		var events = InputMap.action_get_events(action)
+		var events_str = var_to_str(events)
+		
+		config.set_value(INPUT_SECTION, action, events_str)
+	
+	config.save(SETTINGS_FILE)
+
+func reset_to_defaults() -> void:
+	DirAccess.remove_absolute(SETTINGS_FILE)
+	
+	get_tree().quit()
