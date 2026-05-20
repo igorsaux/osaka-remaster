366ba079c4afdae4af6562ed478a08622ec56ce7
igorsaux <55196698+igorsaux@users.noreply.github.com>
2026-05-20 19:14:37 +0300

    refactor camera control

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -15,23 +15,6 @@ class_name FixedOrbitCamera
 @export_range(1.0, 170.0, 0.1, "degrees") var fov_min: float = 10.0
 @export_range(1.0, 170.0, 0.1, "degrees") var fov_max: float = 90.0
 
-@export_group("Mouse Sensitivity")
-@export var mouse_h_sens: float = 0.1
-@export var mouse_v_sens: float = 0.1
-@export var mouse_zoom_speed: float = 2.0
-
-@export_group("Gamepad Sensitivity")
-@export var pad_h_sens: float = 50.0
-@export var pad_v_sens: float = 25.0
-@export var pad_zoom_speed: float = 50.0
-
-@export_group("Trackpad Sensitivity")
-@export var trackpad_pinch_sens: float = 5.0
-@export var trackpad_scroll_sens: float = 0.5
-
-@export_group("Zoom Smoothing")
-@export var zoom_smoothness: float = 10.0
-
 var _curr_x: float = 0.0
 var _curr_y: float = 0.0
 
@@ -46,47 +29,47 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action(GameInputs.CAM_ZOOM_IN) and event.is_pressed():
-		_target_fov -= mouse_zoom_speed
+		_target_fov -= SettingsManager.mouse_zoom_speed
 		_target_fov = clampf(_target_fov, fov_min, fov_max)
 		
 	if event.is_action(GameInputs.CAM_ZOOM_OUT) and event.is_pressed():
-		_target_fov += mouse_zoom_speed
+		_target_fov += SettingsManager.mouse_zoom_speed
 		_target_fov = clampf(_target_fov, fov_min, fov_max)
 
 	if event is InputEventMagnifyGesture:
-		var zoom_delta = (event.factor - 1.0) * trackpad_pinch_sens
+		var zoom_delta = (event.factor - 1.0) * SettingsManager.trackpad_pinch_sens
 
 		_target_fov -= zoom_delta
 		_target_fov = clampf(_target_fov, fov_min, fov_max)
 
 	if event is InputEventPanGesture:
-		var scroll_delta = event.delta.y * trackpad_scroll_sens
+		var scroll_delta = event.delta.y * SettingsManager.trackpad_scroll_sens
 
 		if SettingsManager.invert_trackpad_zoom:
-			_target_fov += scroll_delta
-		else:
 			_target_fov -= scroll_delta
+		else:
+			_target_fov += scroll_delta
 
 		_target_fov = clampf(_target_fov, fov_min, fov_max)
 
 	if event is InputEventMouseMotion:
 		if Input.is_action_pressed(GameInputs.CAM_ROTATE_HOLD):
 			var zoom_compensation = fov / fov_max
-			var delta_y = event.relative.x * mouse_h_sens * zoom_compensation
-			var delta_x = event.relative.y * mouse_v_sens * zoom_compensation
+			var delta_y = event.relative.x * SettingsManager.mouse_h_sens * zoom_compensation
+			var delta_x = event.relative.y * SettingsManager.mouse_v_sens * zoom_compensation
 
 			if SettingsManager.invert_mouse_rotation:
-				_curr_y += delta_y
-				_curr_x += delta_x
-			else:
 				_curr_y -= delta_y
 				_curr_x -= delta_x
+			else:
+				_curr_y += delta_y
+				_curr_x += delta_x
 
 			_apply_rotation_limits()
 
 func _process(delta: float) -> void:
 	if !is_equal_approx(fov, _target_fov):
-		fov = lerpf(fov, _target_fov, minf(1.0, zoom_smoothness * delta))
+		fov = lerpf(fov, _target_fov, minf(1.0, SettingsManager.zoom_smoothness * delta))
 
 	var pad_x = Input.get_axis(GameInputs.CAM_ROT_LEFT, GameInputs.CAM_ROT_RIGHT)
 	var pad_y = Input.get_axis(GameInputs.CAM_ROT_UP, GameInputs.CAM_ROT_DOWN)
@@ -94,14 +77,14 @@ func _process(delta: float) -> void:
 	if pad_x != 0.0 or pad_y != 0.0:
 		var zoom_compensation = fov / fov_max
 
-		_curr_y -= pad_x * pad_h_sens * delta * zoom_compensation
-		_curr_x -= pad_y * pad_v_sens * delta * zoom_compensation
+		_curr_y -= pad_x * SettingsManager.pad_h_sens * delta * zoom_compensation
+		_curr_x -= pad_y * SettingsManager.pad_v_sens * delta * zoom_compensation
 		_apply_rotation_limits()
 
 	var pad_zoom = Input.get_axis(GameInputs.CAM_ZOOM_IN, GameInputs.CAM_ZOOM_OUT)
 
 	if pad_zoom != 0.0:
-		_target_fov += pad_zoom * pad_zoom_speed * delta
+		_target_fov += pad_zoom * SettingsManager.pad_zoom_speed * delta
 		_target_fov = clampf(_target_fov, fov_min, fov_max)
 
 func _apply_rotation_limits() -> void:
--- scripts/settings_manager.gd
+++ scripts/settings_manager.gd
@@ -6,8 +6,17 @@ extends Node
 const SETTINGS_FILE := "user://settings.cfg"
 const INPUT_SECTION := "input"
 
-var invert_mouse_rotation: bool = true
-var invert_trackpad_zoom: bool = true
+var invert_mouse_rotation: bool = false
+var invert_trackpad_zoom: bool = false
+var trackpad_pinch_sens: float = 5.0
+var trackpad_scroll_sens: float = 0.5
+var zoom_smoothness: float = 10.0
+var pad_h_sens: float = 50.0
+var pad_v_sens: float = 25.0
+var pad_zoom_speed: float = 50.0
+var mouse_h_sens: float = 0.1
+var mouse_v_sens: float = 0.1
+var mouse_zoom_speed: float = 2.0
 
 func _ready() -> void:
 	load_settings()
@@ -32,7 +41,17 @@ func load_settings() -> void:
 				for event in events:
 					InputMap.action_add_event(action, event)
 
-		invert_mouse_rotation = config.get_value(INPUT_SECTION, "invert_mouse_rotation", true)
+	invert_mouse_rotation = config.get_value(INPUT_SECTION, "invert_mouse_rotation", false)
+	invert_trackpad_zoom = config.get_value(INPUT_SECTION, "invert_trackpad_zoom", false)
+	trackpad_pinch_sens = config.get_value(INPUT_SECTION, "trackpad_pinch_sens", 5.0)
+	trackpad_scroll_sens = config.get_value(INPUT_SECTION, "trackpad_scroll_sens", 0.5)
+	zoom_smoothness = config.get_value(INPUT_SECTION, "zoom_smoothness", 10.0)
+	pad_h_sens = config.get_value(INPUT_SECTION, "pad_h_sens", 50.0)
+	pad_v_sens = config.get_value(INPUT_SECTION, "pad_v_sens", 25.0)
+	pad_zoom_speed = config.get_value(INPUT_SECTION, "pad_zoom_speed", 50.0)
+	mouse_h_sens = config.get_value(INPUT_SECTION, "mouse_h_sens", 0.1)
+	mouse_v_sens = config.get_value(INPUT_SECTION, "mouse_v_sens", 0.1)
+	mouse_zoom_speed = config.get_value(INPUT_SECTION, "mouse_zoom_speed", 2.0)
 
 func save_settings() -> void:
 	var config = ConfigFile.new()
@@ -44,6 +63,16 @@ func save_settings() -> void:
 		config.set_value(INPUT_SECTION, action, events_str)
 
 	config.set_value(INPUT_SECTION, "invert_mouse_rotation", invert_mouse_rotation)
+	config.set_value(INPUT_SECTION, "invert_trackpad_zoom", invert_trackpad_zoom)
+	config.set_value(INPUT_SECTION, "trackpad_pinch_sens", trackpad_pinch_sens)
+	config.set_value(INPUT_SECTION, "trackpad_scroll_sens", trackpad_scroll_sens)
+	config.set_value(INPUT_SECTION, "zoom_smoothness", zoom_smoothness)
+	config.set_value(INPUT_SECTION, "pad_h_sens", pad_h_sens)
+	config.set_value(INPUT_SECTION, "pad_v_sens", pad_v_sens)
+	config.set_value(INPUT_SECTION, "pad_zoom_speed", pad_zoom_speed)
+	config.set_value(INPUT_SECTION, "mouse_h_sens", mouse_h_sens)
+	config.set_value(INPUT_SECTION, "mouse_v_sens", mouse_v_sens)
+	config.set_value(INPUT_SECTION, "mouse_zoom_speed", mouse_zoom_speed)
 
 	config.save(SETTINGS_FILE)
 
